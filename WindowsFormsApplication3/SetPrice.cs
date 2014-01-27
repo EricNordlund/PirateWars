0c8804d624d9800ad088e759db6ace712fba73ad
EricN <eric@epitet.net>
2014-01-27 16:13:37 +0100

    Setprice partly implemented

---
+++ WindowsFormsApplication3/SetPrice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace PirateWars
+{
+
+    public class SetPrice
+    {
+        private Random rand = new Random();
+
+        //Price configuration
+        private int priceDirectionChance = 35;
+        private int priceChangeMinimumPercentage = 5;
+        private int priceChangeMaximumPercentage = 10;
+
+        
+        //Ändrar priset på en vara. Retunerar det nya priset.
+        public int PriceChangeMove(int initialPrice, bool priceDirectionUp)
+        {
+            int returnPrice;
+            int priceChange = rand.Next(priceChangeMinimumPercentage, priceChangeMaximumPercentage);
+            
+
+            if (rand.Next(0, 100) > priceDirectionChance)
+                priceDirectionUp = !priceDirectionUp;
+
+            if (priceDirectionUp)
+            {
+                priceChange += 100;
+                returnPrice = (initialPrice * priceChange) / 100;
+            }
+            else
+            {
+                priceChange = 100 - priceChange;
+                returnPrice = (initialPrice * priceChange) / 100;
+            }
+            return returnPrice;
+
+        }
+
+    }
+
+}
