9e1cf5f75bffaf91ce43e26ca6d5db7825b4194e
fredrikschon <fredrik_93__@hotmail.com>
2014-01-30 17:39:10 +0100

    Koncept för ExceptionHandler skapad

--- WindowsFormsApplication3/ExceptionHandler.cs
+++ WindowsFormsApplication3/ExceptionHandler.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace PirateWars
 {
     class ExceptionHandler
     {
+
+
+        public void HandleException(String sender, Exception e)
+        {
+            switch (sender)
+            {
+                case "x":
+                    ShowError("");
+                    break;
+                case "y":
+                    ShowError("");
+                    break;
+            }
+        }
+
+        public void ShowError(String error)
+        {
+            MessageBox.Show(error);
+        }
     }
 }
+/*
+ * 
+ try
+ {
+  Göra något
+ }
+ catch(Exception e)
+ {
+   ExceptionHandler.HandleException("Fel på något", e);
+ }
+
+*/
