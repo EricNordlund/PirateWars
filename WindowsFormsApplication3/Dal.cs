dc10585ecadd9aed88cdf7fe747d34493609cd71
EricNordlund <eric@epitet.net>
2014-02-10 14:14:45 +0100

    Added Dal.cs, updated .gitignore to ignore .suo.

---
+++ WindowsFormsApplication3/Dal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace PirateWars
+{
+    public class Dal
+        {
+            SqlConnection connection;
+        
+            public Dal()
+            {
+                String connectionString = "user id=name;" +
+                                          "password=pw;" +
+                                          "server=localhost;" +
+                                          "database=PirateWars;";
+
+                this.connection = new SqlConnection(connectionString);
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+            }
+
+            public SqlDataReader getData(String query)
+            {
+                SqlCommand com = new SqlCommand(query, connection);
+                SqlDataReader dr = null;
+                dr = com.ExecuteReader();
+
+                return dr;
+
+            }
+
+            public void sendData(String query)
+            {
+                SqlCommand com = new SqlCommand(query, connection);
+                com.ExecuteNonQuery();
+            }
+
+        }
+}
+
