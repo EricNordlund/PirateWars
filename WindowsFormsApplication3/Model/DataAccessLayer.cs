fdc3a81dc5b90e4c6e5b3ac1c40950d50615aa96
Lindgren19 <johanlindgren19@hotmail.com>
2014-03-25 09:54:36 +0100

    Highscore
    Lagt till en metod för highscore i DAL.
    
    Nästa steg: Använda metoden vid rätt tillfälle, ex. när dragen är slut.

--- WindowsFormsApplication3/Model/DataAccessLayer.cs
+++ WindowsFormsApplication3/Model/DataAccessLayer.cs
@@ -117,6 +117,16 @@ namespace PirateWars
             return (GetData(query));
         }
 
+        public void AddToHighscoreList(Player player)
+        {
+            string deleteQuery = string.Format("DELETE FROM player WHERE playerName = '{0}';", player.PlayerName);
+            SendData(deleteQuery);
+
+            string queryPlayer = string.Format("INSERT INTO highscore VALUES ( '{0}', '{1}' )", player.PlayerName, player.Gold);
+            SendData(queryPlayer);
+
+        }
+
     }
 }
 
